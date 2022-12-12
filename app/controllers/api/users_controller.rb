273009343a646548520571838704f648b786eca9
emnaruto07 <shazebvps@gmail.com>
2022-12-12 19:43:27 +0300

    first test case pass

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Api
+	class UsersController < ApplicationController
+		def show
+			user = User.find(params[:id])
+
+			respond_to do |format|
+				format.json do
+					render json: user.to_json, status: :ok
+				end
+			end
+		end
+	end
+end
---
+++ spec/requests/api/users_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe "Api::Users", type: :request do
+  describe "GET show" do
+  	let(:headers) do
+  		{ "ACCEPT" => "application/json" }
+  	end
+  	
+  	context "user exists" do
+  		it "is successful" do
+				user =  create(:user)
+				get api_user_path(user), headers: headers
+				expect(response).to be_successful
+  		end
+  	end
+
+  	context "user not exists" do
+			it "is not found" do
+			end
+  	end
+  end
+end
