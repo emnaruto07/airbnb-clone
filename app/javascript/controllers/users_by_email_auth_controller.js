2c858910eeb394ec6524f43ff36a04d52a1083e8
emnaruto07 <shazebvps@gmail.com>
2022-12-13 23:10:07 +0300

    login redirection works!

--- app/javascript/controllers/users_by_email_auth_controller.js
+++ app/javascript/controllers/users_by_email_auth_controller.js
@@ -1,16 +1,29 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
+import axios from 'axios';
 
 export default class extends Controller {
   static targets = ['email', 'submit']
 
   connect() {
+    // console.log("user_by_email", axios)
     this.submitTarget.addEventListener('click', (e) => {
       e.preventDefault();
 
       if (this.emailTarget.value.length === 0){
       } else {
-
+         axios.get('/api/users_by_email', {
+          headers: {
+            'ACCEPT': 'application/json'
+          },
+          params: {
+            email: this.emailTarget.value
+          }
+        }).then((response) => {
+          Turbo.visit('/users/sign_in');
+        }).catch((response) => {
+          Turbo.visit('/users/sign_up');
+        })
       }
     });
-  }
+  };
 }
