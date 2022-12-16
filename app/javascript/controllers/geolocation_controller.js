4766b5e9c244a79250344558933413f744e46b69
emnaruto07 <shazebvps@gmail.com>
2022-12-16 19:20:41 +0300

    calculating geolocation with stimulus #working

--- app/javascript/controllers/geolocation_controller.js
+++ app/javascript/controllers/geolocation_controller.js
@@ -3,16 +3,20 @@ import { getDistance, convertDistance } from 'geolib';
 
 export default class extends Controller {
   static targets = ['property'];
+
   connect() {
     window.navigator.geolocation.getCurrentPosition((position) => {
       this.element.dataset.latitude = position.coords.latitude;
       this.element.dataset.longitude = position.coords.longitude;
+
       this.propertyTargets.forEach((propertyTarget) => {
         // console.log(propertyTarget.querySelector('[data-distance-away]'));
         let distanceFrom = getDistance(
-          { latitude: position.coords.longitude, longitude: position.coords.longitude },
-          { latitude: propertyTarget.dataset.latitude, longitude: propertyTarget.dataset.longitude }
+          { latitude: position.coords.latitude, longitude: position.coords.longitude },
+          { latitude: propertyTarget.dataset.latitude, longitude: propertyTarget.dataset.longitude },
         );
+
+        // console.log(propertyTarget.dataset.latitude)
         propertyTarget.querySelector('[data-distance-away]').innerHTML = `${Math.round(convertDistance(distanceFrom, 'km'))} kilometer away`;
       });
     });
