8ae0950f16b51e812a13fd645d93515639d1081b
emnaruto07 <shazebvps@gmail.com>
2022-12-20 01:39:53 +0300

    fav button working create delete both

--- app/javascript/controllers/favorites_controller.js
+++ app/javascript/controllers/favorites_controller.js
@@ -1,14 +1,35 @@
 import { Controller } from "@hotwired/stimulus";
+import axios from 'axios';
 
 export default class extends Controller {
   // static targets = ['property'];
   favorite(){
+    if (this.element.dataset.userLoggedIn === 'false') {
+      return document.querySelector('[data-header-target="userAuthLink"').click();
+    }
+
     if (this.element.dataset.favorited === 'true'){
-      this.element.dataset.favorited = 'false'
-      this.element.setAttribute('fill', '#ced4da');
-  } else {
-      this.element.dataset.favorited = 'true'
-      this.element.setAttribute('fill', 'black');
+      axios.delete(this.element.dataset.unfavoriteUrl, {
+      headers: {
+        'ACCEPT': 'application/json'
+      }
+      }).then((response) => {
+        this.element.dataset.favorited = 'false'
+        this.element.setAttribute('fill', '#ced4da');
+      });
+    } else {
+      axios.post(this.element.dataset.favoriteUrl, {
+          user_id: this.element.dataset.userId,
+          property_id: this.element.dataset.propertyId
+        },{
+            headers: {
+              'ACCEPT': 'application/json'
+          }
+        })
+      .then((response) => {
+        this.element.dataset.favorited = 'true'
+        this.element.setAttribute('fill', 'black');
+      });
     }
   }
 }
