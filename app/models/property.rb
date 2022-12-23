7e67779535951b481f964ce9b9b7db6b688a5d40
emnaruto07 <shazebvps@gmail.com>
2022-12-23 21:48:02 +0300

    added reservations table

--- app/models/property.rb
+++ app/models/property.rb
@@ -17,6 +17,9 @@ class Property < ApplicationRecord
 	has_many :reviews, as: :reviewable
 	has_many :favorites, dependent: :destroy
 	has_many :favorited_users, through: :favorites, source: :user
+  has_many :reservations, dependent: :destroy
+  has_many :reserved_users, through: :reservations, source: :user
+
 
 	
 
---
+++ spec/factories/reservations.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :reservation do
+    property { nil }
+    user { nil }
+    reservation_date { "2022-12-23" }
+  end
+end
