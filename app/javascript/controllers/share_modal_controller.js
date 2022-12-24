0edbeed4df1d945505bb94097bd7480e16d60b3a
emnaruto07 <shazebvps@gmail.com>
2022-12-25 00:34:46 +0300

    shareCopyLink working.

---
+++ app/javascript/controllers/share_modal_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+
+
+export default class extends Controller {
+	copyLink() {
+		navigator.clipboard.writeText(this.element.dataset.shareUrl);
+	}
+
+	// shareWhatsApp() {
+	// }
+
+	// shareFacebook() {
+	// }
+
+	// sharetwitter() {
+	// }
+}
