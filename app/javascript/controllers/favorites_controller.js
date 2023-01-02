86742299c3be1989c730fb06dee70f9ff0fbe323
emnaruto07 <shazebvps@gmail.com>
2023-01-03 01:27:58 +0300

    to be fixed inside

--- app/javascript/controllers/favorites_controller.js
+++ app/javascript/controllers/favorites_controller.js
@@ -3,7 +3,10 @@ import axios from 'axios';
 
 export default class extends Controller {
   // static targets = ['property'];
-  favorite(){
+  HEADERS = { 'ACCEPT': 'application/json'}
+
+  favorite(e){
+    e.preventDefault()
     if (this.element.dataset.userLoggedIn === 'false') {
       return document.querySelector('[data-header-target="userAuthLink"').click();
     }
@@ -42,4 +45,27 @@ export default class extends Controller {
   getUnfavoritePath(favoriteId){
     return `/api/favorites/${favoriteId}`
   }
+
+// favoriteProperty() {
+//   axios.post(this.getFavoritePath(),{
+//     user_id: this.element.dataset.userId,
+//     property_id: this.element.dataset.propertyId
+//   }, {
+//         headers: HEADERS
+//   }).then((response) => {
+//     this.element.dataset.favorited = 'true'
+//     this.element.dataset.favorited = response.data.id;
+//   });
+// }
+
+// getUnfavoritePath() {
+//   axios.delete(this.getUnfavoritePath(this.element.dataset.favoriteId),{
+//   }, {
+//         headers: this.HEADERS
+//   }).then((response) => {
+//     this.element.dataset.favorited = "false"
+//     this.element.dataset.favoriteId = "";
+//   });
+// }
 }
+
