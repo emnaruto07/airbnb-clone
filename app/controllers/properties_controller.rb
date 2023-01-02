21d72cd9074dcbe398acc08e70c8956afc95f8ba
emnaruto07 <shazebvps@gmail.com>
2023-01-02 22:16:02 +0300

    removing comments

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -2,5 +2,4 @@ class PropertiesController < ApplicationController
   def show
     @property = Property.find(params[:id])
   end
-
 end
--- app/javascript/controllers/share_modal_controller.js
+++ app/javascript/controllers/share_modal_controller.js
@@ -6,12 +6,4 @@ export default class extends Controller {
 		navigator.clipboard.writeText(this.element.dataset.shareUrl);
 	}
 
-	// shareWhatsApp() {
-	// }
-
-	// shareFacebook() {
-	// }
-
-	// sharetwitter() {
-	// }
 }
